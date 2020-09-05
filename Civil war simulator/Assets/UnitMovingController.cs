bb4b9ded2b78ea19bc3e6d1fa305084d58e63bdc
TeslaAngel <42201165+TeslaAngel@users.noreply.github.com>
2020-09-05 23:13:36 +0800

    Demo Tier 1

---
+++ Civil war simulator/Assets/UnitMovingController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class UnitMovingController : MonoBehaviour
+{
+    private NavMeshAgent meshAgent;
+    private MeshRenderer meshRenderer;
+    private SkinnedMeshRenderer skinnedMeshRenderer;
+
+    public bool IsSelected;
+    public bool IsSkinnedMesh;
+    public GameObject MeshObject;
+    public Material Standard;
+    public Material Outline;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshAgent = GetComponent<NavMeshAgent>();
+
+        if (IsSkinnedMesh)
+        {
+            skinnedMeshRenderer = MeshObject.GetComponent<SkinnedMeshRenderer>();
+        }
+        else
+        meshRenderer = MeshObject.GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Mouse Command
+        if (Input.GetMouseButtonDown(0) && IsSelected==true)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject == gameObject)
+                {
+                    IsSelected = false;
+                    if(!IsSkinnedMesh)
+                        meshRenderer.material=Standard;
+                    else
+                        skinnedMeshRenderer.material=Standard;
+                    return;
+                }
+                meshAgent.SetDestination(hit.point);
+                
+            }
+        }
+        if (Input.GetMouseButtonDown(0) && IsSelected == false)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject == gameObject)
+                {
+                    IsSelected = true;
+                    if (!IsSkinnedMesh)
+                        meshRenderer.material=Outline;
+                    else
+                        skinnedMeshRenderer.material=Outline;
+                }
+            }
+        }
+        
+    }
+}
