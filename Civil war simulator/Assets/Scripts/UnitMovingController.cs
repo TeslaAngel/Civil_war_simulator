b7ab551db3353ddf0639b3af84f0d573edc6d32f
TeslaAngel <42201165+TeslaAngel@users.noreply.github.com>
2021-07-23 00:36:50 +0800

    UI and other stuff

---
+++ Civil war simulator/Assets/Scripts/UnitMovingController.cs
@@ -0,0 +1,170 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class UnitMovingController : MonoBehaviour
+{
+    
+    private NavMeshAgent meshAgent;
+    private MeshRenderer meshRenderer;
+    private SkinnedMeshRenderer skinnedMeshRenderer;
+
+    private Animator animator;
+
+    //[Space]
+    [Header("AgentSettings")]
+    //public float MotionGap;
+    //private float MotionGapCalculator = 0;
+    public float MaxTravelDistance;
+    public bool CanMove;
+
+    //[Space]
+    [Header("MeshSettings")]
+    public bool IsSkinnedMesh;
+    public GameObject MeshObject;
+    public Material Standard;
+    public Material Outline;
+
+    [Header("GameSettings")]
+    public bool PermittedToFire;
+    public bool IsSelected;
+    public GameObject DistanceIndicator;
+    public GameObject RangeIndicator;
+
+    [Header("WeaponsSettings")]
+    public List<GameObject> Weapons = new List<GameObject>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Obtaining NavMeshAgent
+        meshAgent = GetComponent<NavMeshAgent>();
+        
+        //Obtaining Animator
+        animator = GetComponent<Animator>();
+
+        //Getting Mesh Component
+        //Defining whatever the object is using SkinnedMeshRenderer or not
+        if (IsSkinnedMesh)
+        {
+            skinnedMeshRenderer = MeshObject.GetComponent<SkinnedMeshRenderer>();
+        }
+        else
+        meshRenderer = MeshObject.GetComponent<MeshRenderer>();
+
+        //Hide DistanceIndicator
+        DistanceIndicator.SetActive(false);
+        //Set the scale of DI in accordance of MaxTravelDistance
+        DistanceIndicator.transform.localScale = new Vector3(MaxTravelDistance, DistanceIndicator.transform.localScale.y, MaxTravelDistance);
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        #region Mouse Command
+        if (Input.GetMouseButtonDown(0) && IsSelected==true)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                //if Mouse Command is deselecting the object
+                if (hit.collider.gameObject == gameObject)
+                {
+                    IsSelected = false;
+                    if(!IsSkinnedMesh)
+                        meshRenderer.material=Standard;
+                    else
+                        skinnedMeshRenderer.material=Standard;
+
+                    DistanceIndicator.SetActive(false);
+                    RangeIndicator.SetActive(false);
+
+                    return;
+                }
+                //if Object is not movable
+                if (!CanMove)//if (MotionGapCalculator > 0)
+                    return;
+                //if Target Location is too far
+                if (Vector3.Distance(transform.position, hit.point) > MaxTravelDistance)
+                    return;
+                meshAgent.SetDestination(hit.point);
+                CanMove = false; //MotionGapCalculator = MotionGap;
+
+            }
+        }
+        if (Input.GetMouseButtonDown(0) && IsSelected == false)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject == gameObject)
+                {
+                    IsSelected = true;
+                    if (!IsSkinnedMesh)
+                        meshRenderer.material=new Material(Outline);
+                    else
+                        skinnedMeshRenderer.material= new Material(Outline);
+
+                    DistanceIndicator.SetActive(true);
+                    RangeIndicator.SetActive(true);
+                }
+            }
+        }
+        #endregion
+
+        #region Mesh Command
+        /*
+        //Calculating Motion Gap
+        if (MotionGapCalculator > 0)
+        {
+            MotionGapCalculator -= Time.deltaTime;
+        }
+        else if (MotionGapCalculator < 0)
+        {
+            MotionGapCalculator = 0;
+        }
+
+        //Visualizing Time Gap Calculator
+
+        float FFvalue = 1 - (1 / MotionGap) * MotionGapCalculator;
+        
+        if (!IsSkinnedMesh && IsSelected)
+        {
+            meshRenderer.material.SetFloat("Vector1_287BDE35", FFvalue);
+        }
+        if (IsSkinnedMesh && IsSelected)
+        {
+            skinnedMeshRenderer.material.SetFloat("Vector1_287BDE35", FFvalue);
+        }
+        */
+
+        //Visualizing if the unit is still able to move
+        float FFvalue;
+        if (CanMove)
+            FFvalue = 1;
+        else
+            FFvalue = 0.1f;
+
+        if (!IsSkinnedMesh && IsSelected)
+        {
+            meshRenderer.material.SetFloat("Vector1_287BDE35", FFvalue);
+        }
+        if (IsSkinnedMesh && IsSelected)
+        {
+            skinnedMeshRenderer.material.SetFloat("Vector1_287BDE35", FFvalue);
+        }
+        #endregion
+
+        #region Animator Command
+        //Animator Sync
+        animator.SetBool("FireReady", PermittedToFire);
+        #endregion
+    }
+}
---
+++ Civil war simulator/Assets/Scripts/WarDefiner.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.AI;
+
+public class WarDefiner : MonoBehaviour
+{
+    [Header("WarInfo")]
+    public List<GameObject> Units = new List<GameObject>();
+    List<UnitMovingController> unitMovingControllers = new List<UnitMovingController>();
+    UnitMovingController unitInControll;
+    public string GameCondition; //Active, Controll, pause
+
+    [Header("Camera (Cinemachine)")]
+    public CinemachineVirtualCamera virtualCamera;
+
+
+    //Sync camera target as the UnitInControll
+    void SyncUnitInControllInRefofCameraTarget()
+    {
+        unitInControll = virtualCamera.m_Follow.gameObject.GetComponent<UnitMovingController>();
+    }
+
+
+    //Method to switch camera target to next unit of the list
+    public void SwitchCameraTargetUnitToNext()
+    {
+        //Getting Index of next unit
+        int nextIndex = Units.IndexOf(virtualCamera.m_Follow.gameObject) + 1;
+
+        if (Units.Count > nextIndex)
+        {
+            //Follow next unit of the list
+            virtualCamera.m_Follow = Units[nextIndex].transform;
+            virtualCamera.m_LookAt = Units[nextIndex].transform;
+        }
+        else
+        {
+            //Follow First unit of the list
+            virtualCamera.m_Follow = Units[0].transform;
+            virtualCamera.m_LookAt = Units[0].transform;
+        }
+        SyncUnitInControllInRefofCameraTarget();
+    }
+
+
+    //Method to switch camera target to previous unit of the list
+    public void SwitchCameraTargetUnitToPrevious()
+    {
+        //Getting Index of previous unit
+        int nextIndex = Units.IndexOf(virtualCamera.m_Follow.gameObject) - 1;
+
+        if (nextIndex >= 0)
+        {
+            //Follow previous unit of the list
+            virtualCamera.m_Follow = Units[nextIndex].transform;
+            virtualCamera.m_LookAt = Units[nextIndex].transform;
+        }
+        else
+        {
+            //Follow Last unit of the list
+            virtualCamera.m_Follow = Units[Units.Count-1].transform;
+            virtualCamera.m_LookAt = Units[Units.Count - 1].transform;
+        }
+        SyncUnitInControllInRefofCameraTarget();
+    }
+
+
+    //Method to Remove a unit from both lists
+    public void RemoveUnitFromLists(GameObject unit)
+    {
+        Units.Remove(unit);
+        unitMovingControllers.Remove(unit.GetComponent<UnitMovingController>());
+
+        //switch camera target if current target is the removing object
+        if(unitInControll.gameObject == unit)
+        {
+            SwitchCameraTargetUnitToPrevious();
+        }
+    }
+
+
+    //Method to turn GameCondition to Controll
+    public void ToControllCondition()
+    {
+        //Make All Units Movable
+        for(int i = 0; i < Units.Count; i++)
+        {
+            unitMovingControllers[i].CanMove = true;
+            unitMovingControllers[i].PermittedToFire = false;
+            Units[i].GetComponent<NavMeshAgent>().isStopped = true;
+        }
+
+        GameCondition = "Controll";
+    }
+
+    //Method to turn GameCondition to Controll
+    public void ToActiveCondition()
+    {
+        //Make All Units Movable
+        for (int i = 0; i < Units.Count; i++)
+        {
+            unitMovingControllers[i].CanMove = false;
+            Units[i].GetComponent<NavMeshAgent>().isStopped = false;
+        }
+
+        GameCondition = "Active";
+    }
+
+
+    public void FirePermission()
+    {
+        //unitInControll.PermittedToFire = true;
+        for (int i = 0; i < Units.Count; i++)
+        {
+            if (unitMovingControllers[i].IsSelected)
+            {
+                unitMovingControllers[i].PermittedToFire = true;
+            }
+        }
+    }
+
+
+    public void StandByCommand()
+    {
+        for (int i = 0; i < Units.Count; i++)
+        {
+            if (unitMovingControllers[i].IsSelected)
+            {
+                Units[i].GetComponent<NavMeshAgent>().SetDestination(Units[i].transform.position);
+                unitMovingControllers[i].CanMove = true;
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //Obtaining Units
+        GameObject[] units = GameObject.FindGameObjectsWithTag("Unit");
+        for(int i = 0; i < units.Length; i++)
+        {
+            Units.Add(units[i]);
+            unitMovingControllers.Add(units[i].GetComponent<UnitMovingController>());
+        }
+
+        //Defining Condition
+        // GameCondition = "Controll";
+        ToControllCondition(); 
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //When active, check to determine when to return to Controll Condition
+        if(GameCondition == "Active")
+        {
+            //Check if all the navigations were finished
+            bool toControll = true;
+            for(int i = 0; i < Units.Count; i++)
+            {
+                if (!(Vector3.Distance(Units[i].transform.position, Units[i].GetComponent<NavMeshAgent>().destination ) <= 1) )
+                //if(Units[i].GetComponent<NavMeshAgent>().isStopped)
+                {
+                    toControll = false;
+                }
+            }
+
+            if (toControll)
+            {
+                print("toControll");
+                ToControllCondition();
+            }
+            
+        }
+    }
+}
